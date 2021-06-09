7ee1736da6a69f0c1eb86fd50d39ff50b5318cd6
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-06-09 14:13:21 +0100

    added headers, paragraphs and spacing to test page

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Header from '../components/Header';
+import Description from '../components/Description';
 import Footer from '../components/Footer';
 
 function About() {
     return (
         <div>
             <Navbar />
-            <Header />
+            <br></br>
+            <Header header='About Myself'/>
+            <Description paragraph='Hello, everyone, welcome to my page! Here is something about me...'/>
+            <br></br>
+            <Header header='My Accomplishments'/>
+            <Description paragraph='Here is a list of some of the stuff I have accomplished:'/>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
             <Footer />
         </div>
     )
