b3809b7cd89632bcd7a7c335fb852cedb69810ea
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-06-09 16:13:59 +0100

    added and styled page intro

---
+++ client/src/components/PageIntro/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+function PageIntro() {
+    return (
+        <div class='page-intro'>
+            <div class='text'>
+                <b>Asad</b> Ansari
+            </div>
+        </div>
+    )
+}
+
+export default PageIntro;
