90051ff7896874b2ad3fd8e595b951bdf8967963
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-06-09 16:34:49 +0100

    added page content

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -12,19 +12,26 @@ function About() {
             <PageIntro />
             <br></br>
             <br></br>
-            <Header header='About Me'/>
+            <Header header='About Me' />
             <br></br>
-            <Description paragraph='Hello, everyone, welcome to my page! Here is something about me...'/>
-            <br></br>
-            <br></br>
-            <Header header='Business'/>
-            <br></br>
-            <Description paragraph='Here is an insider to my business background...'/>
+            <Description paragraph='With vast knowledge of the rapidly changing business world, Asad Ansari is the founder and managing director of multiple risk advisory consultancies, with a wide-ranging experience of a number of sectors including Financial, Legal and Technology.
+            Being well versed in public relations, Asad has been involved in charity organisations within UK and Asia including the British Asian Trust Mental Health Association in Pakistan to raise awareness of mental health. He has also volunteered for Graham Layton Trust, providing free eye care for people of all backgrounds throughout Pakistan.
+            Among other volunteering roles, Asad is on the Armed Forces Muslim Forum communication group committee dealing with interfaith and diversity related issues. Asad is currently involved in the City of London Campaign to become the Commons Councillor.
+            '/>
             <br></br>
             <br></br>
-            <Header header='Philanthropy'/>
+            <Header header='Business' />
             <br></br>
-            <Description paragraph='Here is a list of some of my donations...'/>
+            <Description paragraph='Asad is the Founder and Managing Director of
+            By taking proactive methods to enhance business performance, improving customer service and strengthening credibility, he has specialised in large risk and regulatory projects for clients and companies.
+            Aside from his Consultancy, Asad manages Transform Learning Academy, a Training company offering courses and consultancy in Business Analysis, Financial Crime and GDPR. Asad believes in the vast importance of diversity and the learning agenda for young professionals. He and his partners are passionate about helping individuals to upskill and build their confidence to progress their careers successfully.
+            ' />
+            <br></br>
+            <br></br>
+            <Header header='Philanthropy' />
+            <br></br>
+            <Description paragraph='“Philanthropy is a huge part of my life. It fills a gap for me. The more I help others, the more it pays off personally. In a small way, I’m trying to help make a lasting impact in others’ lives. All the initiatives I’m working on with trustable charities in different sectors, help me do that.”' />
+            <br></br>
             <br></br>
             <br></br>
             <br></br>
