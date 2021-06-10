521a552aeabbdea4680fe2f73f5ba1056691135f
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-06-10 13:44:48 +0100

    added and placed images

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Row, Col } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
 
 // import components here
 import Navbar from '../components/Navbar';
@@ -11,13 +11,34 @@ import PageIntro from '../components/PageIntro';
 
 // import images here
 import Image1 from '../Assets/Asad-Ansari-1280x640.jpg';
+import Logo1 from '../Assets/RiskIntelLogo.png';
+import Logo2 from '../Assets/MayfairITConsultancyLogo.png';
+import Logo3 from '../Assets/AccrediNationLogo.png';
 
-const myStyle1 = {
-    display: 'block',
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    width: '640px',
-    height: '330px'
+const myStyle = {
+    style1: {
+        display: 'block',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        width: '640px',
+        height: '330px'
+    },
+
+    style2: {
+        display: 'block',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        width: '450px',
+        height: '150px'
+    },
+
+    style3: {
+        display: 'block',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        width: '320px',
+        height: '90px'
+    }
 }
 
 function About() {
@@ -30,7 +51,7 @@ function About() {
             <br></br>
             <Header header='About Me' />
             <br></br>
-            <img src={Image1} style={myStyle1}></img><br></br>
+            <img src={Image1} style={myStyle.style1}></img><br></br>
             <Description paragraph='With vast knowledge of the rapidly changing business world, Asad Ansari is the founder and managing director of multiple risk advisory consultancies, with a wide-ranging experience of a number of sectors including Financial, Legal and Technology.' />
             <Description paragraph='Being well versed in public relations, Asad has been involved in charity organisations within UK and Asia including the British Asian Trust Mental Health Association in Pakistan to raise awareness of mental health. He has also volunteered for Graham Layton Trust, providing free eye care for people of all backgrounds throughout Pakistan.' />
             <Description paragraph='Among other volunteering roles, Asad is on the Armed Forces Muslim Forum communication group committee dealing with interfaith and diversity related issues. Asad is currently involved in the City of London Campaign to become the Commons Councillor.' />
@@ -39,6 +60,14 @@ function About() {
             <Header header='Business' />
             <br></br>
             <Description paragraph='Asad is the Founder and Managing Director of:' />
+            <Container>
+                <Row>
+                    <img src={Logo1} style={myStyle.style3}></img>
+                    <img src={Logo2} style={myStyle.style2}></img>
+                    <img src={Logo3} style={myStyle.style3}></img>
+                </Row>
+            </Container>
+            <br></br>
             <Description paragraph='By taking proactive methods to enhance business performance, improving customer service and strengthening credibility, he has specialised in large risk and regulatory projects for clients and companies.' />
             <Description paragraph='Aside from his Consultancy, Asad manages Transform Learning Academy, a Training company offering courses and consultancy in Business Analysis, Financial Crime and GDPR. Asad believes in the vast importance of diversity and the learning agenda for young professionals. He and his partners are passionate about helping individuals to upskill and build their confidence to progress their careers successfully.' />
             <br></br>
