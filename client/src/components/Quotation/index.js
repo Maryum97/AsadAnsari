c234bc6a56adfb1c1526bc45b70e89a16f097bea
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-06-10 14:00:54 +0100

    added component for philanthropy

---
+++ client/src/components/Quotation/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Description from '../Description';
+import { Container, Row, Col } from 'reactstrap';
+import ImageQuote from '../../Assets/Screenshot_20210524-180312_Instagram.jpg'
+import './style.css';
+
+const myStyle = {
+    display: 'block',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        width: '300px',
+        height: '300px'
+}
+
+function Quotation() {
+    return (
+        <Container>
+            <div className='quotation'>
+                <Row>
+                    <Col xs='3'>
+                        <img src={ImageQuote} style={myStyle}></img>
+                    </Col>
+                    <Col xs='9'>
+                        <div className='quote'>
+                            <Description paragraph='“Philanthropy is a huge part of my life. It fills a gap for me. The more I help others, the more it pays off personally. In a small way, I’m trying to help make a lasting impact in others’ lives. All the initiatives I’m working on with trustable charities in different sectors, help me do that."' />
+                        </div>
+                    </Col>
+                </Row>
+            </div>
+        </Container>
+    )
+}
+
+export default Quotation;
