e235e0d83589dfdb658b6c54f8ee1ec5a02eb319
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-06-10 12:19:25 +0100

    edited background color and text

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,44 +1,15 @@
 import React, { useState } from 'react';
 import {
-    Collapse,
     Navbar,
-    NavbarToggler,
-    NavbarBrand,
-    Nav,
-    NavItem,
-    NavLink
+    NavbarBrand
 } from 'reactstrap';
 import './style.css'
 
-const MainNavbar = (props) => {
-    const [isOpen, setIsOpen] = useState(false);
-
-    const toggle = () => setIsOpen(!isOpen);
-
+const MainNavbar = () => {
     return (
         <div>
             <Navbar expand="md" className='navbar'>
-                <NavbarBrand href="/"><b>Asad</b>Ansari</NavbarBrand>
-                <NavbarToggler onClick={toggle} />
-                <Collapse isOpen={isOpen} navbar>
-                    <Nav className="mr-auto" navbar>
-                        <NavItem>
-                            <NavLink href="/">About Me</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink href="/">Business</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink href="/">Philanthropy</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink href="/">Awards</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink href="/">Contact Me</NavLink>
-                        </NavItem>
-                    </Nav>
-                </Collapse>
+                <NavbarBrand href="/"><h1><b>Asad</b>Ansari</h1></NavbarBrand>
             </Navbar>
         </div>
     );
