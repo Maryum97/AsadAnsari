66cf21595d9edb553693e8577cee88d6aceb1e98
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-06-14 16:39:33 +0100

    added JSX content and included images

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Row } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
 
 // import components here
 import Navbar from '../components/Navbar';
@@ -9,13 +9,22 @@ import Description from '../components/Description';
 import Footer from '../components/Footer';
 import PageIntro from '../components/PageIntro';
 import Quotation from '../components/Quotation';
+import PhilCard from '../components/PhilanthropyItems';
 
 // import images here
 import Image1 from '../Assets/Asad-Ansari-1280x640.jpg';
 import Logo1 from '../Assets/RiskIntelLogo.png';
 import Logo2 from '../Assets/MayfairITConsultancyLogo.png';
 import Logo3 from '../Assets/AccrediNationLogo.png';
-import PhilItems from '../components/PhilanthropyItems';
+import Item1 from '../Assets/BritishAsianTrust.png';
+import Item2 from '../Assets/ArmedForcesMuslimAssociation.png';
+import Item3 from '../Assets/GrahamLaytonTrust.png';
+import Item4 from '../Assets/CBH.png';
+import Item5 from '../Assets/IslamChannel.png';
+import Item6 from '../Assets/Deloitte.png';
+import Image2 from '../Assets/Top100AsianStarsInUKTech.png';
+
+document.body.style.backgroundColor = 'rgb(250, 245, 235)'
 
 const myStyle = {
     style1: {
@@ -86,13 +95,61 @@ function About() {
             />
             <Description paragraph='Asad was joined by Michael Mainelli (Sheriff, and Alderman), Mark Wheatley (Common Councillor), and Moawia Bin-Sufyan as he was recognised being a Freeman for the City of London on 23/02/2021 by Laura Miller (Deputy Clerk to the Chamberlain’s Court).' />
             <Description paragraph='Others who have received the Freeman or Free Sister recognition are amongst Royal Family members; HRH Princess Elizabeth, HM Queen Elizabeth The Queen Mother, Diana, Princess of Wales, Prime ministers of the United Kingdom, Presidents of the United States of America, Entrepreneurs and other Senior officials within Royal Family, Politics, Business, and celebrity in their chosen field.' />
-            <Description heading="Asad's other philanthropic work includes the following:" />
+            <Description heading="Asad's other philanthropic works include the following:" />
             <Container>
-                <PhilItems />
+                <Row>
+                    <Col xs='4'>
+                        <PhilCard
+                            item={Item1}
+                            title='British Asian Trust Mental Health Association'
+                            text='From 2018, Asad has supported the British Asian Trust on their mental health projects throughout Pakistan (Asad’s main focus), India and Bangladesh.'
+                        />
+                    </Col>
+                    <Col xs='4'>
+                        <PhilCard
+                            item={Item2}
+                            title='Armed Forces Muslim Forum'
+                            text='Asad has provided advice on diversity-related issues to the Ministry of Defense, working to raise awareness for the Army within the Professional Services sector and The City.'
+                        />
+                    </Col>
+                    <Col xs='4'>
+                        <PhilCard
+                            item={Item3}
+                            title='Graham Layton Trust (LRBT)'
+                            text='From 2017, Asad has volunteered to raise funding for the Graham Layton Trust in partnership with LRBT, providing hospitals and clinics in Pakistan free eye care for all citizens.'
+                        />
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col xs='4'>
+                        <PhilCard
+                            item={Item4}
+                            title='Council of British Hajjis UK'
+                            text='A former member and current brand ambassador, Asad has been helping to end white collar crime within the Hajj and Umrah travel sector.'
+                        />
+                    </Col>
+                    <Col xs='4'>
+                        <PhilCard
+                            item={Item5}
+                            title='Living the Life TV Appearance'
+                            text='In 2019, Asad had a TV appearance on the talk show Living the Life on Islamn Channel, talking about spirituality and faith.'
+                        />
+                    </Col>
+                    <Col xs='4'>
+                        <PhilCard
+                            item={Item6}
+                            title='Deloits Muslim Network'
+                            text='Asad has provided educational initiatives such as diversity and inclusion networks, delivering profile building and connecting corporate companies in the UK.'
+                        />
+                    </Col>
+                </Row>
             </Container>
             <br></br>
             <br></br>
             <Header header='Awards' />
+            <br></br>
+            <img src={Image2} style={myStyle.style1} className="img-fluid"></img><br></br>
             <br></br>
             <Description
                 heading='Asians In Tech Award 2020'
