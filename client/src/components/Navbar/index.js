e37a0e7d601e3e6e08acc2e8e908b870e17f6a87
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-06-14 18:29:37 +0100

    changed second navbar to sticky and nav-links

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -9,7 +9,7 @@ const MainNavbar = () => {
     return (
         <div>
             <Navbar expand="md" className='navbar'>
-                <NavbarBrand href="/"><h2><b>Asad</b>Ansari</h2></NavbarBrand>
+                <NavbarBrand href="/"><h2 className='logo'><b>A</b>A</h2></NavbarBrand>
             </Navbar>
         </div>
     );
--- client/src/components/Navbar2/index.js
+++ client/src/components/Navbar2/index.js
@@ -1,47 +1,20 @@
 import React, { useState } from 'react';
-import {
-    Collapse,
-    Navbar,
-    NavbarToggler,
-    NavbarBrand,
-    Nav,
-    NavItem,
-    NavLink
-} from 'reactstrap';
 import './style.css'
 
-const MainNavbar = (props) => {
-    const [isOpen, setIsOpen] = useState(false);
-
-    const toggle = () => setIsOpen(!isOpen);
+const NavLinks = (props) => {
 
     return (
-        <div>
-            <Navbar color="light" light expand="md" className='navbar2'>
-                <NavbarBrand href="/"></NavbarBrand>
-                <NavbarToggler onClick={toggle} className='mr-2'/>
-                <Collapse isOpen={isOpen} navbar>
-                    <Nav className="mr-auto" navbar>
-                        <NavItem>
-                            <NavLink href="#about">About Me</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink href="#business">Business</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink href="#philanthropy">Philanthropy</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink href="#awards">Awards</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink href="#contact">Contact Me</NavLink>
-                        </NavItem>
-                    </Nav>
-                </Collapse>
-            </Navbar>
+        <div className='card'>
+            <div className='list-group'>
+                <a href="#about" className="list-group-item list-group-item-action">About Me</a>
+                <a href="#business" className="list-group-item list-group-item-action">Business</a>
+                <a href="#philanthropy" className="list-group-item list-group-item-action">Philanthropy</a>
+                <a href="#awards" className="list-group-item list-group-item-action">Awards</a>
+                <a href="#contact" className="list-group-item list-group-item-action">Contact Me</a>
+                <a href='#page' className="list-group-item list-group-item-action">Back To Top</a>
+            </div>
         </div>
     );
 }
 
-export default MainNavbar;
+export default NavLinks;
