61e8603591f2270d98b7a312268f0c467ed93220
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-06-15 15:32:46 +0100

    added functionality for showing/hiding nav

--- client/src/components/Navbar2/index.js
+++ client/src/components/Navbar2/index.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Button } from 'reactstrap';
+import './style.css';
 
 const myStyles = {
     card: {
@@ -14,16 +16,32 @@ const myStyles = {
 }
 
 function NavLinks() {
+    // declaring state variables
+    const [open, setOpen] = useState('false');
+
+    const handleToggle = () => {
+        setOpen(!open);
+    }
 
     return (
-        <div className='card' style={myStyles.card}>
-            <div className='list-group'>
-                <a href="#about" className="list-group-item list-group-item-action" style={myStyles.listGroupItem}>About Me</a>
-                <a href="#business" className="list-group-item list-group-item-action" style={myStyles.listGroupItem}>Business</a>
-                <a href="#philanthropy" className="list-group-item list-group-item-action" style={myStyles.listGroupItem}>Philanthropy</a>
-                <a href="#awards" className="list-group-item list-group-item-action" style={myStyles.listGroupItem}>Awards</a>
-                <a href="#contact" className="list-group-item list-group-item-action" style={myStyles.listGroupItem}>Contact Me</a>
-                <a href='#page' className="list-group-item list-group-item-action" style={myStyles.listGroupItem}>Back To Top</a>
+        <div>
+            <div className='card' style={myStyles.card}>
+                <Button
+                    onClick={handleToggle}
+                    color='primary'
+                    >
+                        show/hide nav
+                </Button>
+                {open ?
+                <div className='list-group'>
+                    <a href="#about" className="list-group-item list-group-item-action" style={myStyles.listGroupItem}>About Me</a>
+                    <a href="#business" className="list-group-item list-group-item-action" style={myStyles.listGroupItem}>Business</a>
+                    <a href="#philanthropy" className="list-group-item list-group-item-action" style={myStyles.listGroupItem}>Philanthropy</a>
+                    <a href="#awards" className="list-group-item list-group-item-action" style={myStyles.listGroupItem}>Awards</a>
+                    <a href="#contact" className="list-group-item list-group-item-action" style={myStyles.listGroupItem}>Contact Me</a>
+                    <a href='#page' className="list-group-item list-group-item-action" style={myStyles.listGroupItem}>Back To Top</a>
+                </div>
+                : null}
             </div>
         </div>
     );
