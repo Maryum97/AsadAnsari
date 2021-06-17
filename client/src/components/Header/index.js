374387e53dea84285c17ddd59563ed95d60c0e4d
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-06-17 16:01:30 +0100

    added/edited styling

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,23 +1,18 @@
 import React, { useEffect } from 'react';
-import Aos from 'aos';
-import 'aos/dist/aos.css';
-import { Container } from 'reactstrap';
 import './style.css';
 
-function Header(props) {
+// import aos dependencies here
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 
-    // when component mounts, apply global animations to the divs
-    useEffect(() => {
-        Aos.init({ duration: 5000 });
-    }, []);
+const Header = (props) => {
+useEffect(() => {
+    Aos.init({ duration: 2000 })
+}, []);
 
     return (
-        <div data-aos='fade-up' className='header'>
-            <div className='header-content'>
-                <Container>
-                    <h1 data-aos='fade-up'><b>{props.header}</b></h1>
-                </Container>
-            </div>
+        <div className='header'>
+                <h1 className='header-text'><b>Hello!</b></h1>
         </div>
     )
 }
