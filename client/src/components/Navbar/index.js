26bad47cf16c636b26f05300a58ad7ee346c4fad
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-06-17 21:09:08 +0100

    edited styles

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -9,7 +9,7 @@ const MainNavbar = () => {
     return (
         <div>
             <Navbar expand="md" className='navbar'>
-                <NavbarBrand href="/"><h1 className='text'><b>Asad</b>Ansari</h1></NavbarBrand>
+                <NavbarBrand href="/"><h1 className='nav-text'><b>Asad</b>Ansari</h1></NavbarBrand>
             </Navbar>
         </div>
     );
