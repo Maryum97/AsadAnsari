a692c6849c10a2f33832d35a2b979e6036923ea8
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-06-17 21:08:55 +0100

    added animation

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -6,13 +6,19 @@ import Aos from 'aos';
 import 'aos/dist/aos.css';
 
 const Header = (props) => {
-useEffect(() => {
-    Aos.init({ duration: 2000 })
-}, []);
+    useEffect(() => {
+        Aos.init({ duration: 1000 })
+    }, []);
 
     return (
-        <div data-aos='fade-left' className='header'>
-                <h1 className='header-text'><b>{props.header}</b></h1>
+        <div
+            data-aos='fade-left'
+            data-aos-easing="ease-in-out"
+            data-aos-mirror="true"
+            data-aos-once="false"
+            className='header'
+            >
+            <h1 className='header-text'><b>{props.header}</b></h1>
         </div>
     )
 }
