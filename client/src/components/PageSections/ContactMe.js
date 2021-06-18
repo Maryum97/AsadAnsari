799827e649c75c1b7cac5efacd0f5f213bcb33fb
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-06-18 13:56:09 +0100

    changed styles and removed header

--- client/src/components/PageSections/ContactMe.js
+++ client/src/components/PageSections/ContactMe.js
@@ -1,14 +1,19 @@
 import React from 'react';
 
 // import components here
-import Header from '../Header';
 import Description from '../Description';
+
+// define styles here
+const myStyles = {
+    contact: {
+        backgroundColor: 'rgb(221, 185, 94)',
+        height: '3000px'
+    }
+}
 
 function ContactMe() {
     return (
-        <div>
-            <Header header='Contact Me' />
-            <br></br>
+        <div className='contact' style={myStyles.contact}>
             <Description
                 paragraph='Feel free to get in touch via my email or social media.'
             />
