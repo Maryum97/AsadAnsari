74dc1aa9b8ac7e3d66c67d9e4225a2fe8ae7d9aa
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-06-20 23:22:33 +0100

    added content to component

--- client/src/components/SocialMedia/index.js
+++ client/src/components/SocialMedia/index.js
@@ -1,22 +1,49 @@
 import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+
+// import contact icons here
+import Facebook from '../../Assets/Contact-Icons/facebook.png';
+import Twitter from '../../Assets/Contact-Icons/twitter.png';
+import Instagram from '../../Assets/Contact-Icons/instagram.png';
+import LinkedIn from '../../Assets/Contact-Icons/linkedin.png'
 
 // define styles here
 const myStyles = {
     style1: {
-        padding: '10%',
+        padding: '22%',
         height: '500px',
         textAlign: 'center',
-        marginTop: '100px'
+        marginTop: '400px'
     }
 }
 
 const SocialMedia = () => {
     return (
         <div style={myStyles.style1}>
-            Facebook
-            Twitter
-            Instagram
-            LinkedIn
+            <Container>
+                <Row>
+                    <Col xs='3'>
+                        <a href='' target='_blank'>
+                            <img className='img-fluid' src={Facebook}></img>
+                        </a>
+                    </Col>
+                    <Col xs='3'>
+                        <a href='' target='_blank'>
+                            <img className='img-fluid' src={Twitter}></img>
+                        </a>
+                    </Col>
+                    <Col xs='3'>
+                        <a href='' target='_blank'>
+                            <img className='img-fluid' src={Instagram}></img>
+                        </a>
+                    </Col>
+                    <Col xs='3'>
+                        <a href='' target='_blank'>
+                            <img className='img-fluid' src={LinkedIn}></img>
+                        </a>
+                    </Col>
+                </Row>
+            </Container>
 
             {/* <Row>
                 <Col xs='12'>
