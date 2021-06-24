48de2f9b9718ebd66d10d5237207dfda05b0594e
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-06-24 23:03:20 +0100

    re-designed navbar and added toggle in media-queries

---
+++ client/src/components/Navbar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title: 'About Me',
+        url: '#about',
+        cName: 'nav-link'
+    },
+    {
+        title: 'Business',
+        url: '#business',
+        cName: 'nav-link'
+    },
+    {
+        title: 'Philanthropy',
+        url: '#philanthropy',
+        cName: 'nav-link'
+    },
+    {
+        title: 'Awards',
+        url: '#awards',
+        cName: 'nav-link'
+    },
+    {
+        title: 'Contact Me',
+        url: '#contact',
+        cName: 'nav-link'
+    }
+]
--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,56 +1,56 @@
-import React, { useState } from 'react';
-import {
-    Navbar,
-    NavbarBrand
-} from 'reactstrap';
-import './style.css';
+// import React, { useState } from 'react';
+// import {
+//     Navbar,
+//     NavbarBrand
+// } from 'reactstrap';
+// import './style.css';
 
-// import other components here
-import Navbar2 from '../Navbar2';
+// // import other components here
+// import Navbar2 from '../Navbar2';
 
-// define styles here
-const myStyles = {
-    navbar: {
-        zIndex: '1',
-        position: 'fixed',
-        marginTop: '0px',
-        width: '100%',
-        height: '110px',
-        maxHeight: '35vh',
-        textAlign: 'center'
-    }
-}
+// // define styles here
+// const myStyles = {
+//     navbar: {
+//         zIndex: '1',
+//         position: 'fixed',
+//         marginTop: '0px',
+//         width: '100%',
+//         height: '110px',
+//         maxHeight: '35vh',
+//         textAlign: 'center'
+//     }
+// }
 
-const MainNavbar = () => {
-    // define state variables here
-    const [colorChange, setColorChange] = useState(false);
+// const MainNavbar = () => {
+//     // define state variables here
+//     const [colorChange, setColorChange] = useState(false);
 
-    // define function to change color
-    const changeNavColor = () => {
-        if (window.scrollY >= 110) {
-            setColorChange(true);
-        }
-        else {
-            setColorChange(false);
-        }
-    }
+//     // define function to change color
+//     const changeNavColor = () => {
+//         if (window.scrollY >= 110) {
+//             setColorChange(true);
+//         }
+//         else {
+//             setColorChange(false);
+//         }
+//     }
 
-    window.addEventListener('scroll', changeNavColor);
+//     window.addEventListener('scroll', changeNavColor);
 
-    return (
-        <div>
-            <Navbar
-                expand="md"
-                className={colorChange ? 'navbar active' : 'navbar'}
-                style={myStyles.navbar}
-            >
-                <NavbarBrand href="/"><h1 className='nav-text'><b>Asad</b>Ansari</h1></NavbarBrand>
-            </Navbar>
+//     return (
+//         <div>
+//             <Navbar
+//                 expand="md"
+//                 className={colorChange ? 'navbar active' : 'navbar'}
+//                 style={myStyles.navbar}
+//             >
+//                 <NavbarBrand href="/"><h1 className='nav-text'><b>Asad</b>Ansari</h1></NavbarBrand>
+//             </Navbar>
 
-            <Navbar2 />
+//             <Navbar2 />
 
-        </div>
-    );
-}
+//         </div>
+//     );
+// }
 
-export default MainNavbar;
+// export default MainNavbar;
