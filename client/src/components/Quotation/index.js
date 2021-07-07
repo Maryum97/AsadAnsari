262e27ca9470aaff688b0332316fb2c3151d47b9
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-07-07 14:11:15 +0100

    removed rows and cols, and added media queries via externale stylesheet

--- client/src/components/Quotation/index.js
+++ client/src/components/Quotation/index.js
@@ -27,41 +27,41 @@ function Quotation() {
                 data-aos-once="false"
                 data-aos-delay='200'
             >
-                <Row>
-                    <Col xs='4'>
-                        <br></br>
-                        <img
-                            src={ImageQuote}
-                            style={myStyle.styleImage}
-                            className="img-fluid"
-                            data-aos='zoom-in'
-                            data-aos-easing="ease-in-out"
-                            data-aos-mirror="true"
-                            data-aos-once="false"
-                            data-aos-delay="700"
-                        >
 
-                        </img>
-                    </Col>
-                    <Col xs='8'>
-                        <div
-                            className='quote'
-                            style={myStyle.styleQuote}
-                            data-aos='flip-up'
-                            data-aos-easing="ease-in-out"
-                            data-aos-mirror="true"
-                            data-aos-once="false"
-                            data-aos-delay="1300"
-                        >
-                            <Description
-                                paragraph='“Philanthropy is a huge part of my life. It fills a gap for me. The more I help others, the more it pays off personally. In a small way, I’m trying to help make a lasting impact in others’ lives. All the initiatives I’m working on with trustable charities in different sectors, help me do that."'
-                            />
-                            <Description paragraph='~ Asad Ansari' />
-                        </div>
-                    </Col>
-                </Row>
+                <div className='quote-image'>
+                    <br></br>
+                    <img
+                        src={ImageQuote}
+                        style={myStyle.styleImage}
+                        className="img-fluid"
+                        data-aos='zoom-in'
+                        data-aos-easing="ease-in-out"
+                        data-aos-mirror="true"
+                        data-aos-once="false"
+                        data-aos-delay="700"
+                    >
+
+                    </img>
+                </div>
+
+                <div className='quote-text'>
+                    <div
+                        className='quote'
+                        style={myStyle.styleQuote}
+                        data-aos='flip-up'
+                        data-aos-easing="ease-in-out"
+                        data-aos-mirror="true"
+                        data-aos-once="false"
+                        data-aos-delay="1300"
+                    >
+                        <Description
+                            paragraph='“Philanthropy is a huge part of my life. It fills a gap for me. The more I help others, the more it pays off personally. In a small way, I’m trying to help make a lasting impact in others’ lives. All the initiatives I’m working on with trustable charities in different sectors, help me do that."'
+                        />
+                        <Description paragraph='~ Asad Ansari' />
+                    </div>
+                </div>
             </div>
-        </Container>
+        </Container >
     )
 }
 
