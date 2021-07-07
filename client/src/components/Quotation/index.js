072a2175a6f7d89150cc8d3a46729e642d71fd7a
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-07-07 13:39:49 +0100

    added on-scroll effects

--- client/src/components/Quotation/index.js
+++ client/src/components/Quotation/index.js
@@ -5,23 +5,57 @@ import ImageQuote from '../../Assets/Screenshot_20210524-180312_Instagram.jpg'
 import './style.css';
 
 const myStyle = {
-    display: 'block',
+    styleImage: {
+        display: 'block',
         marginLeft: 'auto',
-        marginRight: 'auto',
+        marginRight: 'auto'
+    },
+    styleQuote: {
+        fontWeight: '500',
+        fontStyle: 'italic'
+    }
 }
 
 function Quotation() {
     return (
         <Container>
-            <div className='quotation'>
+            <div
+                className='quotation'
+                data-aos='fade-up'
+                data-aos-easing="ease-in-out"
+                data-aos-mirror="true"
+                data-aos-once="false"
+                data-aos-delay='200'
+            >
                 <Row>
                     <Col xs='4'>
                         <br></br>
-                        <img src={ImageQuote} style={myStyle} className="img-fluid"></img>
+                        <img
+                            src={ImageQuote}
+                            style={myStyle.styleImage}
+                            className="img-fluid"
+                            data-aos='zoom-in'
+                            data-aos-easing="ease-in-out"
+                            data-aos-mirror="true"
+                            data-aos-once="false"
+                            data-aos-delay="700"
+                        >
+
+                        </img>
                     </Col>
                     <Col xs='8'>
-                        <div className='quote'>
-                            <Description paragraph='“Philanthropy is a huge part of my life. It fills a gap for me. The more I help others, the more it pays off personally. In a small way, I’m trying to help make a lasting impact in others’ lives. All the initiatives I’m working on with trustable charities in different sectors, help me do that."' />
+                        <div
+                            className='quote'
+                            style={myStyle.styleQuote}
+                            data-aos='flip-up'
+                            data-aos-easing="ease-in-out"
+                            data-aos-mirror="true"
+                            data-aos-once="false"
+                            data-aos-delay="1300"
+                        >
+                            <Description
+                                paragraph='“Philanthropy is a huge part of my life. It fills a gap for me. The more I help others, the more it pays off personally. In a small way, I’m trying to help make a lasting impact in others’ lives. All the initiatives I’m working on with trustable charities in different sectors, help me do that."'
+                            />
                             <Description paragraph='~ Asad Ansari' />
                         </div>
                     </Col>
